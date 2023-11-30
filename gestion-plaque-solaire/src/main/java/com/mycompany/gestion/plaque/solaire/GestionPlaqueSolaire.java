ee6c39bda293116d09538a440d46075f9dfec96a
01KingMaker <fabrakoto0987@gmail.com>
2023-11-30 08:39:38 +0300

    initial commit

---
+++ gestion-plaque-solaire/src/main/java/com/mycompany/gestion/plaque/solaire/GestionPlaqueSolaire.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestion.plaque.solaire;
+
+/**
+ *
+ * @author fabien
+ */
+public class GestionPlaqueSolaire {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
